12c384c7a7b46520a6461b5438021c9af1061801
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-29 08:42:41 -0500

    feat: :art: Add component html structure, tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import avatar from "./assets/images/avatar-michelle.jpg";
+import drawers from "./assets/images/drawers.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="w-full h-screen flex justify-center items-center">
+        <div className="w-[700px] h-[400px] flex md:flex-row-reverse">
+          <div className="">
+            <div>
+              <h2>
+                Shift the overall look and feel by adding these wonderful
+                touches to furniture in your home
+              </h2>
+              <p>
+                Ever been in a room and felt like something was missing? Perhaps
+                it felt slightly bare and uninviting. I’ve got some simple tips
+                to help you make any room feel complete.
+              </p>
+            </div>
+            <div>
+              <div>
+                <img src={avatar} alt="avatar" />
+                <div>
+                  <h3>Michelle Appleton</h3>
+                  <p>28 Jun 2020</p>
+                </div>
+              </div>
+              <div>row</div>
+            </div>
+          </div>
+          <img src={drawers} alt="picture"  className="w-[30%]"/>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
