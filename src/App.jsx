0ba41a9d88514b93802eb8d2b953fac08ed2c025
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-10-02 22:37:40 -0500

    feat: :art: add avatar and Tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,37 @@ function App() {
   return (
     <>
       <main className="w-full h-screen flex justify-center items-center">
-        <div className="w-[700px] h-[400px] flex md:flex-row-reverse">
-          <div className="">
-            <div>
-              <h2>
+        <div className="w-[45.625rem] h-[17.5rem] flex md:flex-row-reverse bg-white">
+          <div className="px-[2.5rem] py-[2rem]">
+            <div className="">
+              <h2 className="text-[1.25rem] font-bold text-dark-grayish-blue">
                 Shift the overall look and feel by adding these wonderful
                 touches to furniture in your home
               </h2>
-              <p>
+              <p className="text-[.8125rem] text-desaturated-dark-blue mt-[.75rem]">
                 Ever been in a room and felt like something was missing? Perhaps
                 it felt slightly bare and uninviting. I’ve got some simple tips
                 to help you make any room feel complete.
               </p>
             </div>
-            <div>
-              <div>
-                <img src={avatar} alt="avatar" />
-                <div>
+            <div className="flex justify-between mt-[1.25rem]">
+              <div className="flex gap-[1.25rem] items-center">
+                <img src={avatar} alt="avatar"  className="w-[2.5rem] h-[2.5rem] rounded-full"/>
+                <div className="text-[.8125rem]">
                   <h3>Michelle Appleton</h3>
-                  <p>28 Jun 2020</p>
+                  <p className="text-grayish-blue">28 Jun 2020</p>
                 </div>
               </div>
               <div>row</div>
             </div>
           </div>
-          <img src={drawers} alt="picture"  className="w-[30%]"/>
+          <img
+            src={drawers}
+            alt="picture"W
+            width={540}
+            height={446}
+            className="w-[39%] object-cover object-left"
+          />
         </div>
       </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'desaturated-dark-blue': 'hsl(214, 17%, 51%)',
+        'grayish-blue': 'hsl(212, 23%, 69%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)',
+      },
+      colors: {
+        'dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'desaturated-dark-blue': 'hsl(214, 17%, 51%)',
+        'grayish-blue': 'hsl(212, 23%, 69%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)',
+      },
+    },
   },
   plugins: [],
 }
